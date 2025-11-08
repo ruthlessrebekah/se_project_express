60d6a432bfa8c3de7ccb33f073fca5e91968d77f
Ruthie <ruthlessrebekah@gmail.com>
2025-11-08 10:47:25 -0600

    Update CORS config and environment variables

--- app.js
+++ app.js
@@ -29,15 +29,14 @@ app.use(
     optionsSuccessStatus: 204,
   })
 );
-app.use(cors());
-app.options("*", cors());
+
 app.use(cookieParser());
 app.use(requestLogger);
 
 const { PORT = 3001 } = process.env;
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/wtwr_db")
   .then(() => {
     // console.log("Connected to MongoDB successfully");
   })
